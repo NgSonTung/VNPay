322177709f9c348a2232bfc910539547097feaaf
NgSonTung <ngsondecon@gmail.com>
2022-07-09 20:22:28 +0700

    test

--- App.js
+++ App.js
@@ -4,12 +4,13 @@ import HomeScreen from './app/screens/HomeScreen';
 import LoginScreen from './app/screens/LoginScreen';
 import NTHeader from './app/components/NTHeader'
 import NTHead from './app/screens/NapTien';
+import Notification from './app/components/Notification';
 
 
 export default function App() {
   return (
     //<HomeScreen/>
-    <NTHead />
+    <Notification />
   );
 }
 
