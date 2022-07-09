4665a938e73c9e9877fff6170b7d95d21000c0d8
Maanhi1311 <nguyenmaianh13112003@gmail.com>
2022-07-09 20:37:30 +0700

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './app/screens/HomeScreen';
 import LoginScreen from './app/screens/LoginScreen';
+import Notification from './app/components/Notification';
 import NTHeader from './app/components/NTHeader'
 import NTHead from './app/screens/NapTien';
 
@@ -9,7 +10,8 @@ import NTHead from './app/screens/NapTien';
 export default function App() {
   return (
     //<HomeScreen/>
-    <NTHead />
+    //<NTHead />
+    <Notification/>
   );
 }
 
