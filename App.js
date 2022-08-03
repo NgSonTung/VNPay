3f392b28074915f395e06b9ba13646a436a33a80
Tuo238 <billbui3002@gmail.com>
2022-08-03 19:01:28 +0700

    Add page Chat

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import CTDenViPage from "./app/screens/ChuyenTienDenVi";
 import CTPage from "./app/screens/ChuyenTien";
 import Notification from "./app/screens/Notification";
 import Gift from "./app/screens/Gift";
+import ChatPage from "./app/screens/Chat"
 import { Text, StyleSheet, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -85,157 +86,165 @@ export default function App() {
     );
   else
     return (
-      <>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="home">
-            <Stack.Screen options={{ headerShown: false }} name="home">
-              {(props) => (
-                <HomeScreen
-                  navigation={props.navigation}
-                  balance={accountInfo.balance}
-                  loggedIn={loggedIn}
-                  name={accountInfo.name}
-                  accountInfo={accountInfo}
-                />
-              )}
-            </Stack.Screen>
-            <Stack.Screen options={{ headerShown: false }} name="login">
-              {(props) => (
-                <LoginScreen
-                  navigation={props.navigation}
-                  number={accountInfo.number}
-                  password={accountInfo.password}
-                  setLogin={setLogin}
-                  accountList={accountList}
-                  setAccountInfo={setAccountInfo}
-                  loggedIn={loggedIn}
-                />
-              )}
-            </Stack.Screen>
-            <Stack.Screen options={{ headerShown: false }} name="naptien">
-              {(props) => (
-                <>
-                  <Load />
-                  <NTHead
-                    navigation={props.navigation}
-                    balance={accountInfo.balance}
-                  />
-                </>
-              )}
-            </Stack.Screen>
-            <Stack.Screen options={{ headerShown: false }} name="xacnhanNT">
-              {(props) => (
-                <>
-                  <Load />
-                  <XNPage
-                    navigation={props.navigation}
-                    balance={accountInfo.balance}
-                    name={accountInfo.name}
-                    setAccountInfo={setAccountInfo}
-                  />
-                </>
-              )}
-            </Stack.Screen>
+      // <>
+      //   <NavigationContainer>
+      //     <Stack.Navigator initialRouteName="home">
+      //       <Stack.Screen options={{ headerShown: false }} name="home">
+      //         {(props) => (
+      //           <HomeScreen
+      //             navigation={props.navigation}
+      //             balance={accountInfo.balance}
+      //             loggedIn={loggedIn}
+      //             name={accountInfo.name}
+      //             accountInfo={accountInfo}
+      //           />
+      //         )}
+      //       </Stack.Screen>
+      //       <Stack.Screen options={{ headerShown: false }} name="login">
+      //         {(props) => (
+      //           <LoginScreen
+      //             navigation={props.navigation}
+      //             number={accountInfo.number}
+      //             password={accountInfo.password}
+      //             setLogin={setLogin}
+      //             accountList={accountList}
+      //             setAccountInfo={setAccountInfo}
+      //             loggedIn={loggedIn}
+      //           />
+      //         )}
+      //       </Stack.Screen>
+      //       <Stack.Screen options={{ headerShown: false }} name="naptien">
+      //         {(props) => (
+      //           <>
+      //             <Load />
+      //             <NTHead
+      //               navigation={props.navigation}
+      //               balance={accountInfo.balance}
+      //             />
+      //           </>
+      //         )}
+      //       </Stack.Screen>
+      //       <Stack.Screen options={{ headerShown: false }} name="xacnhanNT">
+      //         {(props) => (
+      //           <>
+      //             <Load />
+      //             <XNPage
+      //               navigation={props.navigation}
+      //               balance={accountInfo.balance}
+      //               name={accountInfo.name}
+      //               setAccountInfo={setAccountInfo}
+      //             />
+      //           </>
+      //         )}
+      //       </Stack.Screen>
 
-            <Stack.Screen options={{ headerShown: false }} name="myQR">
-              {(props) => (
-                <>
-                  <Load />
-                  <MyQrScreen
-                    navigation={props.navigation}
-                    balance={accountInfo.balance}
-                    name={accountInfo.name}
-                    loggedIn={loggedIn}
-                    accountInfo={accountInfo}
-                  />
-                </>
-              )}
-            </Stack.Screen>
-            <Stack.Screen options={{ headerShown: false }} name="viGD">
-              {(props) => (
-                <>
-                  <Load />
-                  <ViGD name={accountInfo.name} navigation={props.navigation} />
-                </>
-              )}
-            </Stack.Screen>
-            <Stack.Screen options={{ headerShown: false }} name="thongbao">
-              {(props) => (
-                <>
-                  <Load />
-                  <Notification navigation={props.navigation} />
-                </>
-              )}
-            </Stack.Screen>
-            <Stack.Screen options={{ headerShown: false }} name="chuyentien">
-              {(props) => (
-                <>
-                  <Load />
-                  <CTPage navigation={props.navigation} />
-                </>
-              )}
-            </Stack.Screen>
-            <Stack.Screen options={{ headerShown: false }} name="qua">
-              {(props) => (
-                <>
-                  <Load />
-                  <Gift navigation={props.navigation} />
-                </>
-              )}
-            </Stack.Screen>
-            <Stack.Screen options={{ headerShown: false }} name="xacnhan">
-              {(props) => (
-                <>
-                  <Load />
-                  <XNPage
-                    accountInfo={accountInfo}
-                    receiver={receiver}
-                    sendAmount={sendAmount}
-                    navigation={props.navigation}
-                    balance={accountInfo.balance}
-                    name={accountInfo.name}
-                  />
-                </>
-              )}
-            </Stack.Screen>
-            <Stack.Screen
-              options={{ headerShown: false }}
-              name="chuyentiendenvi"
-            >
-              {(props) => (
-                <>
-                  <Load />
-                  <CTDenViPage
-                    navigation={props.navigation}
-                    balance={accountInfo.balance}
-                    name={accountInfo.name}
-                    accountInfo={accountInfo}
-                    accountList={accountList}
-                    setReceiver={setReceiver}
-                    setAmount={setAmount}
-                  />
-                </>
-              )}
-            </Stack.Screen>
-            <Stack.Screen options={{ headerShown: false }} name="menu">
-              {(props) => (
-                <>
-                  <Load />
-                  <MenuScreen
-                    navigation={props.navigation}
-                    balance={accountInfo.balance}
-                    loggedIn={loggedIn}
-                    name={accountInfo.name}
-                    setLogin={setLogin}
-                    setAccountInfo={setAccountInfo}
-                  />
-                </>
-              )}
-            </Stack.Screen>
-          </Stack.Navigator>
-        </NavigationContainer>
-        <Load />
-      </>
+      //       <Stack.Screen options={{ headerShown: false }} name="myQR">
+      //         {(props) => (
+      //           <>
+      //             <Load />
+      //             <MyQrScreen
+      //               navigation={props.navigation}
+      //               balance={accountInfo.balance}
+      //               name={accountInfo.name}
+      //               loggedIn={loggedIn}
+      //               accountInfo={accountInfo}
+      //             />
+      //           </>
+      //         )}
+      //       </Stack.Screen>
+      //       <Stack.Screen options={{ headerShown: false }} name="viGD">
+      //         {(props) => (
+      //           <>
+      //             <Load />
+      //             <ViGD name={accountInfo.name} navigation={props.navigation} />
+      //           </>
+      //         )}
+      //       </Stack.Screen>
+      //       <Stack.Screen options={{ headerShown: false }} name="thongbao">
+      //         {(props) => (
+      //           <>
+      //             <Load />
+      //             <Notification navigation={props.navigation} />
+      //           </>
+      //         )}
+      //       </Stack.Screen>
+      //       <Stack.Screen options={{ headerShown: false }} name="chuyentien">
+      //         {(props) => (
+      //           <>
+      //             <Load />
+      //             <CTPage navigation={props.navigation} />
+      //           </>
+      //         )}
+      //       </Stack.Screen>
+      //       <Stack.Screen options={{ headerShown: false }} name="qua">
+      //         {(props) => (
+      //           <>
+      //             <Load />
+      //             <Gift navigation={props.navigation} />
+      //           </>
+      //         )}
+      //       </Stack.Screen>
+      //       <Stack.Screen options={{ headerShown: false }} name="Chat">
+      //       {(props) => (
+      //         <>
+      //           <Load />
+      //           <ChatPage navigation={props.navigation}/>
+      //         </>)}
+      //     </Stack.Screen>
+      //       <Stack.Screen options={{ headerShown: false }} name="xacnhan">
+      //         {(props) => (
+      //           <>
+      //             <Load />
+      //             <XNPage
+      //               accountInfo={accountInfo}
+      //               receiver={receiver}
+      //               sendAmount={sendAmount}
+      //               navigation={props.navigation}
+      //               balance={accountInfo.balance}
+      //               name={accountInfo.name}
+      //             />
+      //           </>
+      //         )}
+      //       </Stack.Screen>
+      //       <Stack.Screen
+      //         options={{ headerShown: false }}
+      //         name="chuyentiendenvi"
+      //       >
+      //         {(props) => (
+      //           <>
+      //             <Load />
+      //             <CTDenViPage
+      //               navigation={props.navigation}
+      //               balance={accountInfo.balance}
+      //               name={accountInfo.name}
+      //               accountInfo={accountInfo}
+      //               accountList={accountList}
+      //               setReceiver={setReceiver}
+      //               setAmount={setAmount}
+      //             />
+      //           </>
+      //         )}
+      //       </Stack.Screen>
+      //       <Stack.Screen options={{ headerShown: false }} name="menu">
+      //         {(props) => (
+      //           <>
+      //             <Load />
+      //             <MenuScreen
+      //               navigation={props.navigation}
+      //               balance={accountInfo.balance}
+      //               loggedIn={loggedIn}
+      //               name={accountInfo.name}
+      //               setLogin={setLogin}
+      //               setAccountInfo={setAccountInfo}
+      //             />
+      //           </>
+      //         )}
+      //       </Stack.Screen>
+      //     </Stack.Navigator>
+      //   </NavigationContainer>
+      //   <Load />
+      // </>
+      <ChatPage/>
     );
 }
 const styles = StyleSheet.create({
